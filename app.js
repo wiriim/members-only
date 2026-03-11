a2711bae62b3341a8c1d1e585acb16863c3860e3
William Halim <51820876+wiriim@users.noreply.github.com>
2026-03-12 00:05:28 +0700

    add home page layout

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = new express();
+const path = require("node:path");
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// Configuting view with ejs
+app.set("views", path.join(__dirname, "Views"));
+app.set("view engine", "ejs");
 
+// Setting the static assets path
+const assetPath = path.join(__dirname, "Public");
+app.use(express.static(assetPath));
+
+// Routes
+app.get('/', (req, res) => res.render('home'));
+
+// Running / Listening the application
 const DEFAULT_PORT = process.env.DEFAULT_PORT;
 app.listen(DEFAULT_PORT, (err) => {
     if (err)
